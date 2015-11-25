49c03f8990dbb6da45880cae941b0ab92be07269
jonni21 <jhnzvzd@gmail.com>
2015-11-25 16:05:52 +0200

    Personal Trainer

---
+++ src/task16/Human.java
@@ -0,0 +1,51 @@
+package task16;
+
+public class Human {
+
+    private String sex;
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+}
---
+++ src/task16/Main.java
@@ -0,0 +1,10 @@
+package task16;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Consultant consultant = new Consultant();
+        consultant.start();
+    }
+
+}
