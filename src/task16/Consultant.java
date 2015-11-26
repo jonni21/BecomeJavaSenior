42fd2c7e098e6e9711e165f683a3f913f187e81a
jonni21 <jhnzvzd@gmail.com>
2015-11-26 14:42:10 +0200

    1. Объявил константы в классе Human 2. Перенес метод для заполнения полей экземпляра класса Human с консоли в класс Human 3. В связи с этим переписал логику в классе Consultant

--- src/task16/Consultant.java
+++ src/task16/Consultant.java
@@ -8,113 +8,118 @@ public class Consultant {
 
     public void start() {
 
-        Human client = getHuman();
+        System.out.println("My name is Peter, and I'm your personal Fitness Consultant.");
+        System.out.println("I can give you advises about your lifestyle according to your fitness level.");
+        System.out.println("To start with, I need to get some information about you.");
+        Human client = new Human();
+        client.setHumanFromConsole();
+
         BmiCalculator bmiCalc = new BmiCalculator();
         double bmiIndex = bmiCalc.calculateBmi(client.getHeight(), client.getWeight());
         String bmiCategory = bmiCalc.getBmiCategory(bmiIndex);
-        System.out.println("Hello, " + client.getName() + "!");
-        System.out.println("My name is Peter, and I'm your personal Fitness Consultant.");
+        System.out.println("OK, " + client.getName() + "!");
+
         System.out.println("Thank you for the information provided. Let's see what we've got. ");
-        System.out.println("OK. Your BMI Index is: " + bmiIndex);
+        System.out.println("Your BMI Index is: " + bmiIndex);
         System.out.println("Your BMI Category is: " + bmiCategory);
     }
 
-    public Human getHuman() {
-
-        String name;
-        String sex;
-        int age;
-        double height;
-        double weight;
-
-        Human human = new Human();
-
-        System.out.println("Enter your name:");
-        name = getName(input.next());
-        System.out.println("Enter your sex (male/female):");
-        sex = getSex(input.next());
-        System.out.println("Enter your age:");
-        age = getAge(input.next());
-        System.out.println("Enter your height in cm:");
-        height = getHeight(input.next());
-        System.out.println("Enter your weight in kg:");
-        weight = getWeight(input.next());
-
-
-        human.setName(name);
-        human.setSex(sex);
-        human.setAge(age);
-        human.setHeight(height);
-        human.setWeight(weight);
-
-        return human;
-    }
-
-    private String getName(String s) {
-        while ((!s.matches("[a-zA-Z]{3,15}"))) {
-            System.out.println("Enter correct value (3 to 15 letters):");
-            s = input.next();
-        }
-        return s;
-    }
-
-    private String getSex(String sex) {
-        sex = sex.toLowerCase();
-        while (!(sex.matches("male|female"))) {
-            System.out.println("Please enter correct value (male or female):");
-            sex = input.next().toLowerCase();
-        }
-        return sex;
-    }
-
-    private int getAge(String s) {
-        while (true) {
-            while (!(s.matches("\\d+"))) {
-                System.out.println("Enter correct value from 10 to 100:");
-                s = input.next();
-            }
-            int age = Integer.valueOf(s);
-            if ((age >= 10) && (age <= 100)) {
-                return age;
-            } else {
-                System.out.println("Enter correct value from 10 to 100:");
-                s = input.next();
-            }
-        }
-
-    }
-
-    private double getHeight(String s) {
-        while (true) {
-            while (!(s.matches("\\d+[\\.,]?\\d+"))) {
-                System.out.println("Enter correct value from 50 to 250:");
-                s = input.next();
-            }
-            s = s.replace(",", ".");
-            double height = Double.valueOf(s);
-            if ((height >= 50.0) && (height <= 250.0)) {
-                return height;
-            } else {
-                System.out.println("Enter correct value from 50 to 250:");
-                s = input.next();
-            }
-        }
-    }
-
-    private double getWeight(String s) {
-        while (true) {
-            while (!(s.matches("\\d+[\\.,]?\\d+"))) {
-                System.out.println("Enter correct value from 30.0 to 300.0:");
-                s = input.next();
-            }
-            s = s.replace(",", ".");
-            double weight = Double.valueOf(s);
-            if ((weight >= 30.0) && (weight <= 300.0)) {
-                return weight;
-            } else {
-                System.out.println("Enter correct value from 30.0 to 300.0:");
-                s = input.next();
-            }
-        }
-    }
+//    public Human getHuman() {
+//
+//        String name;
+//        String sex;
+//        int age;
+//        double height;
+//        double weight;
+//
+//        Human human = new Human();
+//
+//        System.out.println("Enter your name:");
+//        name = getName(input.next());
+//        System.out.println("Enter your sex (male/female):");
+//        sex = getSex(input.next());
+//        System.out.println("Enter your age:");
+//        age = getAge(input.next());
+//        System.out.println("Enter your height in cm:");
+//        height = getHeight(input.next());
+//        System.out.println("Enter your weight in kg:");
+//        weight = getWeight(input.next());
+//
+//
+//        human.setName(name);
+//        human.setSex(sex);
+//        human.setAge(age);
+//        human.setHeight(height);
+//        human.setWeight(weight);
+//
+//        return human;
+//    }
+//
+//    private String getName(String s) {
+//        while ((!s.matches("[a-zA-Z]{3,15}"))) {
+//            System.out.println("Enter correct value (3 to 15 letters):");
+//            s = input.next();
+//        }
+//        return s;
+//    }
+//
+//    private String getSex(String sex) {
+//        sex = sex.toLowerCase();
+//        while (!(sex.matches("male|female"))) {
+//            System.out.println("Please enter correct value (male or female):");
+//            sex = input.next().toLowerCase();
+//        }
+//        return sex;
+//    }
+//
+//    private int getAge(String s) {
+//        while (true) {
+//            while (!(s.matches("\\d+"))) {
+//                System.out.println("Enter correct value from 10 to 100:");
+//                s = input.next();
+//            }
+//            int age = Integer.valueOf(s);
+//            if ((age >= 10) && (age <= 100)) {
+//                return age;
+//            } else {
+//                System.out.println("Enter correct value from 10 to 100:");
+//                s = input.next();
+//            }
+//        }
+//
+//    }
+//
+//    private double getHeight(String s) {
+//        while (true) {
+//            while (!(s.matches("\\d+[\\.,]?\\d+"))) {
+//                System.out.println("Enter correct value from 50 to 250:");
+//                s = input.next();
+//            }
+//            s = s.replace(",", ".");
+//            double height = Double.valueOf(s);
+//            if ((height >= 50.0) && (height <= 250.0)) {
+//                return height;
+//            } else {
+//                System.out.println("Enter correct value from 50 to 250:");
+//                s = input.next();
+//            }
+//        }
+//    }
+//
+//    private double getWeight(String s) {
+//        while (true) {
+//            while (!(s.matches("\\d+[\\.,]?\\d+"))) {
+//                System.out.println("Enter correct value from 30.0 to 300.0:");
+//                s = input.next();
+//            }
+//            s = s.replace(",", ".");
+//            double weight = Double.valueOf(s);
+//            if ((weight >= 30.0) && (weight <= 300.0)) {
+//                return weight;
+//            } else {
+//                System.out.println("Enter correct value from 30.0 to 300.0:");
+//                s = input.next();
+//            }
+//        }
+//    }
 }
--- src/task16/Human.java
+++ src/task16/Human.java
@@ -1,5 +1,7 @@
 package task16;
 
+import java.util.Scanner;
+
 public class Human {
 
     private String sex;
@@ -47,5 +49,89 @@ public class Human {
     public void setHeight(double height) {
         this.height = height;
     }
+
+    public void setHumanFromConsole() {
+
+        final int MIN_AGE = 10;
+        final int MAX_AGE = 100;
+        final double MIN_HEIGHT = 50.0;
+        final double MAX_HEIGHT = 250.0;
+        final double MIN_WEIGHT = 30.0;
+        final double MAX_WEIGHT = 300.0;
+
+        String s;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        s = input.next();
+        while ((!s.matches("[a-zA-Z]{3,15}"))) {
+            System.out.println("Enter correct value (3 to 15 letters):");
+            s = input.next();
+        }
+        this.name = s;
+
+        System.out.println("Enter your sex (male/female):");
+        s = input.next().toLowerCase();
+        while (!(s.matches("male|female"))) {
+            System.out.println("Please enter correct value (male or female):");
+            s = input.next().toLowerCase();
+        }
+        this.sex = s;
+
+        System.out.println("Enter your age:");
+        s = input.next();
+        while (true) {
+            while (!(s.matches("\\d+"))) {
+                System.out.println("Enter correct value from 10 to 100:");
+                s = input.next();
+            }
+            int age = Integer.valueOf(s);
+            if ((age >= MIN_AGE) && (age <= MAX_AGE)) {
+                this.age = age;
+                break;
+            } else {
+                System.out.println("Enter correct value from 10 to 100:");
+                s = input.next();
+            }
+        }
+
+        System.out.println("Enter your height in cm:");
+        s = input.next();
+        while (true) {
+            while (!(s.matches("\\d+[\\.,]?\\d+"))) {
+                System.out.println("Enter correct value from 50 to 250:");
+                s = input.next();
+            }
+            s = s.replace(",", ".");
+            double height = Double.valueOf(s);
+            if ((height >= MIN_HEIGHT) && (height <= MAX_HEIGHT)) {
+                this.height = height;
+                break;
+            } else {
+                System.out.println("Enter correct value from 50 to 250:");
+                s = input.next();
+            }
+        }
+
+        System.out.println("Enter your weight in kg:");
+        s = input.next();
+        while (true) {
+            while (!(s.matches("\\d+[\\.,]?\\d+"))) {
+                System.out.println("Enter correct value from 30.0 to 300.0:");
+                s = input.next();
+            }
+            s = s.replace(",", ".");
+            double weight = Double.valueOf(s);
+            if ((weight >= MIN_WEIGHT) && (weight <= MAX_WEIGHT)) {
+                this.weight = weight;
+                break;
+            } else {
+                System.out.println("Enter correct value from 30.0 to 300.0:");
+                s = input.next();
+            }
+        }
+
+    }
+
 
 }
