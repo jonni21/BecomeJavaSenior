1fde0695d6cebeed5d3b2002dd0ada4234fa118a
jonni21 <jhnzvzd@gmail.com>
2015-12-03 14:33:44 +0200

    Validation of the input values was added to the setters

--- src/task16/Human.java
+++ src/task16/Human.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Human {
 
+    final int MIN_AGE = 10;
+    final int MAX_AGE = 100;
+    final double MIN_HEIGHT = 50.0;
+    final double MAX_HEIGHT = 250.0;
+    final double MIN_WEIGHT = 30.0;
+    final double MAX_WEIGHT = 300.0;
+
     private String sex;
     private String name;
     private int age;
@@ -14,122 +21,65 @@ public class Human {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public boolean setName(String name) {
+        if (name.matches("[a-zA-Z]{3,15}")) {
+            this.name = name;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public String getSex() {
         return sex;
     }
 
-    public void setSex(String sex) {
-        this.sex = sex;
+    public boolean setSex(String sex) {
+        if (sex.matches("male|female")) {
+            this.sex = sex;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public boolean setAge(int age) {
+        if ((age >= MIN_AGE) && (age <= MAX_AGE)) {
+            this.age = age;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public double getWeight() {
         return weight;
     }
 
-    public void setWeight(double weight) {
-        this.weight = weight;
+    public boolean setWeight(double weight) {
+        if ((weight >= MIN_WEIGHT) && (weight <= MAX_WEIGHT)) {
+            this.weight = weight;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public double getHeight() {
         return height;
     }
 
-    public void setHeight(double height) {
-        this.height = height;
+    public boolean setHeight(double height) {
+        if ((height >= MIN_HEIGHT) && (height <= MAX_HEIGHT)) {
+            this.height = height;
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public void setHumanFromConsole() {
-
-        final int MIN_AGE = 10;
-        final int MAX_AGE = 100;
-        final double MIN_HEIGHT = 50.0;
-        final double MAX_HEIGHT = 250.0;
-        final double MIN_WEIGHT = 30.0;
-        final double MAX_WEIGHT = 300.0;
-
-        String s;
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter your name:");
-        s = input.next();
-        while ((!s.matches("[a-zA-Z]{3,15}"))) {
-            System.out.println("Enter correct value (3 to 15 letters):");
-            s = input.next();
-        }
-        this.name = s;
-
-        System.out.println("Enter your sex (male/female):");
-        s = input.next().toLowerCase();
-        while (!(s.matches("male|female"))) {
-            System.out.println("Please enter correct value (male or female):");
-            s = input.next().toLowerCase();
-        }
-        this.sex = s;
-
-        System.out.println("Enter your age:");
-        s = input.next();
-        while (true) {
-            while (!(s.matches("\\d+"))) {
-                System.out.println("Enter correct value from "+MIN_AGE+" to "+MAX_AGE+":");
-                s = input.next();
-            }
-            int age = Integer.valueOf(s);
-            if ((age >= MIN_AGE) && (age <= MAX_AGE)) {
-                this.age = age;
-                break;
-            } else {
-                System.out.println("Enter correct value from "+MIN_AGE+" to "+MAX_AGE+":");
-                s = input.next();
-            }
-        }
-
-        System.out.println("Enter your height in cm:");
-        s = input.next();
-        while (true) {
-            while (!(s.matches("\\d+[\\.,]?\\d+"))) {
-                System.out.println("Enter correct value from "+MIN_HEIGHT+" to "+MAX_HEIGHT+":");
-                s = input.next();
-            }
-            s = s.replace(",", ".");
-            double height = Double.valueOf(s);
-            if ((height >= MIN_HEIGHT) && (height <= MAX_HEIGHT)) {
-                this.height = height;
-                break;
-            } else {
-                System.out.println("Enter correct value from "+MIN_HEIGHT+" to "+MAX_HEIGHT+":");
-                s = input.next();
-            }
-        }
-
-        System.out.println("Enter your weight in kg:");
-        s = input.next();
-        while (true) {
-            while (!(s.matches("\\d+[\\.,]?\\d+"))) {
-                System.out.println("Enter correct value from "+MIN_WEIGHT+" to "+MAX_WEIGHT+":");
-                s = input.next();
-            }
-            s = s.replace(",", ".");
-            double weight = Double.valueOf(s);
-            if ((weight >= MIN_WEIGHT) && (weight <= MAX_WEIGHT)) {
-                this.weight = weight;
-                break;
-            } else {
-                System.out.println("Enter correct value from "+MIN_WEIGHT+" to "+MAX_WEIGHT+":");
-                s = input.next();
-            }
-        }
-
-    }
 }
